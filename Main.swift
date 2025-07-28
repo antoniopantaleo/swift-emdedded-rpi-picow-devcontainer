cbeb5119ce8d6f1b3e1b1185264c031209539f23
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-07-28 18:43:51 +0200

    refactor: split main function in separate functions

--- Main.swift
+++ Main.swift
@@ -11,24 +11,15 @@
 
 @main
 struct Main {
+
+  private static let led = UInt32(CYW43_WL_GPIO_LED_PIN)
+
   static func main() {
-    let led = UInt32(CYW43_WL_GPIO_LED_PIN)
-    if cyw43_arch_init() != 0 {
-      print("Wi-Fi init failed")
-      return
+
+    guard cyw43_arch_init() == 0 else {
+      return print("Wi-Fi init failed")
     }
-    let dot = {
-      cyw43_arch_gpio_put(led, true)
-      sleep_ms(250)
-      cyw43_arch_gpio_put(led, false)
-      sleep_ms(250)
-    }
-    let dash = {
-      cyw43_arch_gpio_put(led, true)
-      sleep_ms(500)
-      cyw43_arch_gpio_put(led, false)
-      sleep_ms(250)
-    }
+
     while true {
       dot()
       dot()
@@ -43,4 +34,19 @@ struct Main {
       dot()
     }
   }
+
+  private static func dot() {
+    cyw43_arch_gpio_put(led, true)
+    sleep_ms(250)
+    cyw43_arch_gpio_put(led, false)
+    sleep_ms(250)
+  }
+
+  private static func dash() {
+    cyw43_arch_gpio_put(led, true)
+    sleep_ms(500)
+    cyw43_arch_gpio_put(led, false)
+    sleep_ms(250)
+  }
+  
 }
